0c5391066146da91f479822913779716932207ad
Evan Chan <evan.chanyiksan@gmail.com>
2016-11-28 17:03:33 -0500

    change from postgresql to sqlite3

--- webapps/settings.py
+++ webapps/settings.py
@@ -93,14 +93,14 @@ WSGI_APPLICATION = 'webapps.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'CSS',
-        'USER': 'postgres',
-        'PASSWORD': '28003300',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        # 'NAME': 'CSS',
+        # 'USER': 'postgres',
+        # 'PASSWORD': '28003300',
+        # 'HOST': 'localhost',
+        # 'PORT': '',
     }
 }
 
